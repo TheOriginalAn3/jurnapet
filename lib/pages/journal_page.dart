d66fb0de576cd77819a64e6cc91eba264fa2467e
Andrei Gubani <andrei.gubani22@gmail.com>
2025-01-16 19:31:57 +0100

    added dots to bullet list

--- lib/pages/journal_page.dart
+++ lib/pages/journal_page.dart
@@ -1,3 +1,6 @@
+// TODO: Make the date and day of the week stop scrolling when they hit the top of the screen
+// TODO: Make the bullt-list dot icon display at the beginning of the text input, not in the center of the length of the text input
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:jurnapet/pages/journalTextInput_page.dart';
@@ -35,132 +38,149 @@ class _JournalPageState extends State<JournalPage> {
     String dayOfWeek = DateFormat('EEEE').format(currentDate);
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: _padding,
-              child: Text(
-                formattedDate,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Times New Roman",
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: _padding,
-              child: Text(
-                dayOfWeek,
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Times New Roman",
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.only(
-                left: ((_paddingAmmount / 2) - 5),
-                right: _paddingAmmount,
-              ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.circle,
-                    size: 10,
-                    color: Colors.grey.shade400,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: _padding,
+                child: Text(
+                  formattedDate,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Times New Roman",
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: _paddingAmmount / 2 - 5,
-                      top: _paddingAmmount,
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: _padding,
+                child: Text(
+                  dayOfWeek,
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Times New Roman",
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(
+                  left: ((_paddingAmmount / 2) - 5),
+                  right: _paddingAmmount,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.circle,
+                      size: 8,
+                      color: Colors.grey.shade600,
                     ),
-                  ),
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () async {
-                        final newText = await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => JournalTextInputPage(),
-                          ),
-                        );
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: _paddingAmmount / 2 - 5,
+                        top: _paddingAmmount,
+                      ),
+                    ),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () async {
+                          final newText = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => JournalTextInputPage(),
+                            ),
+                          );
 
-                        if (newText != null && newText.toString().isNotEmpty) {
-                          addJournalEntry(newText);
-                        }
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey.shade400,
+                          if (newText != null &&
+                              newText.toString().isNotEmpty) {
+                            addJournalEntry(newText);
+                          }
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.grey.shade400,
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            'Start typing...',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic,
+                              fontFamily: 'Times New Roman',
+                              color: Colors.grey.shade500,
                             ),
                           ),
                         ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ...journalEntries.asMap().entries.map((entry) {
+                int index = entry.key;
+                String text = entry.value;
+                return Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(
+                    left: _paddingAmmount / 2 - 5,
+                    right: _paddingAmmount,
+                    top: 10,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        size: 8,
+                        color: Colors.grey.shade600,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: _paddingAmmount / 2 - 5),
+                      ),
+                      GestureDetector(
+                        onLongPress: () {
+                          setState(() {
+                            journalEntries.removeAt(index);
+                          });
+                        },
+                        onTap: () async {
+                          final updatedText = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => JournalTextInputPage(
+                                initialText: text, // Pass current text to edit
+                              ),
+                            ),
+                          );
+
+                          if (updatedText != null &&
+                              updatedText.toString().isNotEmpty) {
+                            setState(() {
+                              journalEntries[index] = updatedText;
+                            });
+                          }
+                        },
                         child: Text(
-                          'Start typing...',
+                          text,
                           style: TextStyle(
                             fontSize: 16,
-                            fontStyle: FontStyle.italic,
-                            fontFamily: 'Times New Roman',
-                            color: Colors.grey.shade500,
+                            color: Colors.black,
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            ...journalEntries.asMap().entries.map((entry) {
-              int index = entry.key;
-              String text = entry.value;
-              return Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.only(
-                  left: _paddingAmmount,
-                  right: _paddingAmmount,
-                  top: 10,
-                ),
-                child: GestureDetector(
-                  onTap: () async {
-                    final updatedText = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => JournalTextInputPage(
-                          initialText: text, // Pass current text to edit
-                        ),
-                      ),
-                    );
-
-                    if (updatedText != null &&
-                        updatedText.toString().isNotEmpty) {
-                      setState(() {
-                        journalEntries[index] = updatedText;
-                      });
-                    }
-                  },
-                  child: Container(
-                    child: Text(
-                      text,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'Times New Roman',
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            }).toList(),
-          ],
+                );
+              }).toList(),
+            ],
+          ),
         ),
       ),
     );
